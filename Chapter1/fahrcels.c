fadf467f327f06856f650f68e26133d09e00cb32
EngineerFergus <williamskei97@gmail.com>
2022-12-27 12:17:24 -0500

    added fahrenheit to celsius program from 1.2

---
+++ Chapter1/fahrcels.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* print Farhrenheit-Celsius table
+    for fahr = 0, 20, ..., 300 */
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Fahrenheit >> Celsius\n");
+    while (fahr <= upper)
+    {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%10.0f >> %7.2f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
