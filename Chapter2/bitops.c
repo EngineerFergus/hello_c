de0738dcd5bed41b0a19731d500af628ddc8f002
EngineerFergus <williamskei97@gmail.com>
2023-01-02 15:59:13 -0500

    finished bitwise operations

--- Chapter2/bitops.c
+++ Chapter2/bitops.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Exercises 2-6, 2-7, and 2-8 */
+
 int getline(char line[], int limit);
 unsigned setbits(unsigned x, int p, int n, unsigned y);
+unsigned invert(unsigned x, int p, int n);
+int rightrot(int x, int n);
+void testrightrot(int x, int n);
 
 int main()
 {
@@ -22,6 +27,21 @@ int main()
     itoa(a, buffer, 2);
     printf("final     : %s\n", buffer);
     printf("\n");
+
+    printf("  INVERT TEST \n");
+    itoa(b, buffer, 2);
+    printf("starting x: %s\n", buffer);
+    a = invert(b, 3, 4);
+    itoa(a, buffer, 2);
+    printf("inverted  : %s\n", buffer);
+    itoa(1287, buffer, 2);
+    printf("starting x: %s\n", buffer);
+    a = invert(1287, 6, 4);
+    itoa(a, buffer, 2);
+    printf("inverted  : %s\n", buffer);
+
+    testrightrot(123123123, 1);
+    testrightrot(052614, 5);
 
     return 0;
 }
@@ -55,4 +75,31 @@ unsigned setbits(unsigned x, int p, int n, unsigned y)
     mask = (mask << (p + 1 - n)); // shift mask from far right to position p
     r = ((r & mask) | (x & ~mask));
     return r;
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+    unsigned mask;
+    mask = ~(~0 << n); // set up mask
+    mask = (mask << (p + 1 - n));
+    return (mask & ~x) | (~mask & x);
+}
+
+int rightrot(int x, int n)
+{
+    int sz = sizeof(x) * 8;
+    int r = x >> n;
+    int rLeft = x << (sz - n);
+    return r | rLeft;
+}
+
+void testrightrot(int x, int n)
+{
+    char buffer[33];
+    printf("RIGHTROT TEST\n");
+    itoa(x, buffer, 2);
+    printf("Start: %s\n", buffer);
+    int temp = rightrot(x, n);
+    itoa(temp, buffer, 2);
+    printf("Rot  : %s\n", buffer);
 }
