84412be2b2ddf99cf27f86f95995cadce25f292a
EngineerFergus <williamskei97@gmail.com>
2023-01-02 08:31:21 -0500

    got string to hexadecimal working

--- Chapter2/htoi.c
+++ Chapter2/htoi.c
@@ -6,10 +6,26 @@
 
 int atoi(char s[]);
 int htoi(char s[]);
+int convert(char c);
+int getline(char line[], int limit);
+
+#define LIMIT 32 /* max line length */
 
 int main()
 {
+    char line[LIMIT];
+    int i, n;
 
+    for (i = 0; i < LIMIT; i++)
+    {
+        line[i] = '\0';
+    }
+
+    while((i = getline(line, LIMIT)) != 0)
+    {
+        n = htoi(line);
+        printf("%d\n", n);
+    }
 
     return 0;
 }
@@ -30,4 +46,57 @@ int atoi(char s[])
 int htoi(char s[])
 {
     // look up ctype functions that will hopefully help out!
+    int i, n;
+
+    n = 0;
+
+    for (i = 0; s[i] != '\0'; ++i)
+    {
+        if (i == 0 && s[i] == '0')
+        {
+            continue;
+        }
+
+        if (i == 1 && (s[i] == 'x' || s[i] == 'X'))
+        {
+            continue;
+        }
+
+        if (isxdigit(s[i]))
+        {
+            n = 16 * n + convert(s[i]);
+        }
+    }
+
+    return n;
+}
+
+int convert(char c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return c - '0';
+    }
+
+    char lower = tolower(c);
+    return (lower - 'a') + 10;
+}
+
+int getline(char line[], int limit)
+{
+    int c, i;
+
+    for (i=0; i<limit-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    {
+        line[i] = c;
+    }
+
+    if (c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+    return i;
 }
