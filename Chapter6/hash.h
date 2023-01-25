a431621b70cebd4ae48900c79378390a12c81f3c
EngineerFergus <williamskei97@gmail.com>
2023-01-24 21:37:15 -0500

    added source files containing hashmap for exercises 6-5 and 6-6

---
+++ Chapter6/hash.h
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdlib.h>
+
+/* Table entry */
+struct nlist 
+{
+    struct nlist *next; /* next entry in chain */
+    char *name; /* defined name */
+    char *defn; /* replacement text */
+};
+
+/* form hash value for string s */
+unsigned hash(char *s);
+
+/* look for s in hashtab */
+struct nlist *lookup(char *s);
+
+/* put (name, defn) in hashtab */
+struct nlist *install(char *name, char *defn);
