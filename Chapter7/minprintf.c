2cac11743f5643bbdb5a04ca4b4d91e219278254
EngineerFergus <williamskei97@gmail.com>
2023-01-28 09:53:05 -0500

    finished exercise 7-3

---
+++ Chapter7/minprintf.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void minprintf(char *fmt, ...);
+
+int main()
+{
+    char *fmt = "the %s has location (%d, %d) and distance %f";
+    char *pt = "point";
+    int x = 4;
+    int y = 5;
+    double dist = 3.14159;
+
+    minprintf(fmt, pt, x, y, dist);
+
+    return 0;
+}
+
+/* minimal printf with variable argument list */
+void minprintf(char *fmt, ...)
+{
+    va_list ap; /* points to each unnamed arg in turn */
+    char *p, *sval;
+    int ival;
+    double dval;
+
+    va_start(ap, fmt); /* make ap point to 1st unnamed arg */
+
+    for (p = fmt; *p; p++)
+    {
+        if (*p != '%')
+        {
+            putchar(*p);
+            continue;
+        }
+
+        switch (*++p)
+        {
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                break;
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; sval++)
+                {
+                    putchar(*sval);
+                }
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+
+    va_end(ap);
+}
